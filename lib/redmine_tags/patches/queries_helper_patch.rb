ef6bb1c559c26ee9b046ce2ed25bcb40bf09e992
Tiago Ceridorio <tiagoceridorio@gmail.com>
2020-03-01 17:24:06 -0300

    melhorias no Código e Testes

--- lib/redmine_tags/patches/queries_helper_patch.rb
+++ lib/redmine_tags/patches/queries_helper_patch.rb
@@ -1,3 +1,5 @@
+require_dependency 'queries_helper'
+
 module RedmineTags
   module Patches
     module QueriesHelperPatch
@@ -9,18 +11,23 @@ module RedmineTags
         include TagsHelper
 
         def column_content(column, item)
-          if column.name == :tags
-            column.value(item).collect{ |t| render_tag_link(t) }
-              .join(RedmineTags.settings[:issues_use_colors].to_i > 0 ? ' ' : ', ').html_safe
+          begin
+            value = column.value_object(item)
+          rescue
+            value = nil
+          end
+          if column.name == :tags && value.present? && value.kind_of?(ActiveRecord::Associations::CollectionProxy)
+            value = value.to_i
+            value.collect {|t| render_tag_link(t)}.compact.join(RedmineTags.settings[:issues_use_colors].to_i > 0 ? ' ' : ', ').html_safe
           else
             super
           end
         end
 
         def csv_content(column, issue)
-          value = column.value_object(issue)
-          if column.name == :tags
-            value.collect {|v| csv_value(column, issue, v)}.compact.join(', ').html_safe
+          if column.name == :related_projects && column.value_object(issue).kind_of?(ActiveRecord::Associations::CollectionProxy)
+            value = column.value_object(issue).to_a
+            value.collect {|v| csv_value(column, issue, v)}.compact.join(', ')
           else
             super
           end
