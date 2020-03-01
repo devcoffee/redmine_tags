9c09bd4a7155d97738d9e86e89f99217de39131f
Tiago Ceridorio <tiagoceridorio@gmail.com>
2020-03-01 16:59:07 -0300

    Ajustes Colunas de Tag

--- lib/redmine_tags/patches/queries_helper_patch.rb
+++ lib/redmine_tags/patches/queries_helper_patch.rb
@@ -3,33 +3,26 @@ module RedmineTags
     module QueriesHelperPatch
       def self.included(base)
         base.send :include, InstanceMethods
-        base.class_eval do
-          alias_method :column_content_without_redmine_tags, :column_content
-          alias_method :column_content, :column_content_with_redmine_tags
-
-          alias_method :csv_content_without_redmine_tags, :csv_content
-          alias_method :csv_content, :csv_content_with_redmine_tags
-        end
       end
 
       module InstanceMethods
         include TagsHelper
 
-        def column_content_with_redmine_tags(column, issue)
+        def column_content(column, item)
           if column.name == :tags
             column.value(issue).collect{ |t| render_tag_link(t) }
               .join(RedmineTags.settings[:issues_use_colors].to_i > 0 ? ' ' : ', ').html_safe
           else
-            column_content_without_redmine_tags(column, issue)
+            super
           end
         end
 
-        def csv_content_with_redmine_tags(column, issue)
+        def csv_content(column, issue)
           value = column.value_object(issue)
           if column.name == :tags
             value.collect {|v| csv_value(column, issue, v)}.compact.join(', ').html_safe
           else
-            csv_content_without_redmine_tags(column, issue)
+            super
           end
         end
       end
